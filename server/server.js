a6fd082c661d2d14da2bb983867d601ea109f935
Bethelhem Ashenafi <bettyashu5@gmail.com>
2026-04-25 11:37:13 +0300

    update

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 // Socket.io setup
 const io = new Server(server, {
-  cors: { origin: "http://localhost:3000" },
+  cors: { origin: "https://ethio-bid-auction-system.vercel.app" },
 });
 
 app.set("io", io); // make io accessible in routes
@@ -29,7 +29,7 @@ io.on("connection", (socket) => {
 });
 
 // Middleware
-app.use(cors({ origin: "http://localhost:3000", methods: ["GET","POST","PUT","DELETE","PATCH"] }));
+app.use(cors({ origin: "https://ethio-bid-auction-system.vercel.app", methods: ["GET","POST","PUT","DELETE","PATCH"] }));
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
